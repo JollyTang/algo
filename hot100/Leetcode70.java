8cc10a78b7edd2062af85dc49df51f680119fc7f
jollytang <2578614022@qq.com>
2025-08-09 15:02:05 +0800

    70

---
+++ hot100/Leetcode70.java
@@ -0,0 +1,16 @@
+package hot100;
+
+public class Leetcode70 {
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n + 1]; // dp[n] 爬上 n的方法 dp[n] = dp[n-1] + dp[n-2]
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
