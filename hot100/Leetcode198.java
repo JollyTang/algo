33eeb780406b5a866b6a0d5dbf75cc01569b15bf
jollytang <2578614022@qq.com>
2025-08-09 15:19:27 +0800

    198

---
+++ hot100/Leetcode198.java
@@ -0,0 +1,24 @@
+package hot100;
+
+// 输入：[1,2,3,1]
+// 输出：4
+// dp[n] = max(dp[n-1],dp[n-2] + num[n])
+
+public class Leetcode198 {
+    public int rob(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        if (nums.length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return dp[nums.length - 1];
+    }
+}
