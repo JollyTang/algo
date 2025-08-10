de4f70b7efa9a60535dea4b4b65d569aa2c35f62
jollytang <2578614022@qq.com>
2025-08-10 23:27:31 +0800

    41

--- hot100/Leetcode41.java
+++ hot100/Leetcode41.java
@@ -3,27 +3,29 @@ package hot100;
 // 输入：nums = [1,2,0] 1-N
 // 输出：3
 // 解释：范围 [1,2] 中的数字都在数组中。
-
+// 1 -> index = 0
+// i -> index = i-1
+// nums[i] 当前下标为i的合法数据
+// nums[i]-1 他要去的下标位置
+// nums[nums[i]-1] 下标位置当前的数据 
 public class Leetcode41 {
     public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
         for (int i = 0; i < nums.length; i++) {
-            int val = nums[i];
-            if (val > 0 && val <= nums.length && val - 1 != i) {
-                swap(nums, val - 1, i);
+            while (nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+                // int temp = nums[i];
+                // nums[i] = nums[nums[i] - 1];
+                // nums[nums[i] - 1] = temp;
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
             }
         }
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < n; i++) {
             if (nums[i] - 1 != i) {
-                return nums[i];
+                return i + 1;
             }
         }
-        return nums.length + 1;
+        return n + 1;
     }
-
-    public void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-
 }
